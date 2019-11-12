864057bc2e4dd4b518f05d2960a1adbf71d05f34
Freshia <freshiasackey@gmail.com>
2019-11-12 13:29:43 +0300

    First commit of GSM SMS code

---
+++ Communication/GSM.ino
@@ -0,0 +1,139 @@
+
+
+#include <TinyGsmClient.h>
+
+//change this if you are using a different modem
+#define TINY_GSM_MODEM_SIM800
+
+#define TINY_GSM_DEBUG SerialMon
+
+// Range to attempt to autobaud
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 38400
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+#define SerialAT Serial1
+#define TINY_GSM_USE_GPRS true
+
+#define TINY_GSM_USE_SMS true
+
+//for pin unlock if your sim card is pin enabled
+#define GSM_PIN ""
+
+// Set phone number to send SMS to
+#define SMS_TARGET  "+254XXXXXXXXX"
+
+const char apn[]  = "YOUR APN";
+const char gprs_User[] = "";
+const char gprs_Pass[] = "";
+
+
+
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+void setup() {
+  SerialMon.begin(115200);
+  delay(10);
+
+  DBG("Wait...");
+
+  TinyGsmAutoBaud(SerialAT,GSM_AUTOBAUD_MIN,GSM_AUTOBAUD_MAX);
+  delay(2500);
+}
+
+void loop() {
+
+  initialize_GSM_and_send_SMS();
+  delay(1000);
+}
+void initialize_GSM_and_send_SMS(){
+  // Restart takes longer. To skip it, call init() instead of restart()
+  DBG("Initializing modem...");
+  if (!modem.restart()) {
+    DBG("Failed to restart modem, delaying 10s and retrying");
+    delay(3000);
+    // restart autobaud in case GSM just rebooted
+    TinyGsmAutoBaud(SerialAT,GSM_AUTOBAUD_MIN,GSM_AUTOBAUD_MAX);
+    delay(10000);
+    return;
+  }
+
+  String name = modem.getModemName();
+  DBG("Modem Name:", name);
+
+  String modemInfo = modem.getModemInfo();
+  DBG("Modem Info:", modemInfo);
+
+#if TINY_GSM_USE_GPRS
+  // SIM unlock
+  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(GSM_PIN);
+  }
+#endif
+
+  DBG("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    delay(10000);
+    return;
+  }
+
+  if (modem.isNetworkConnected()) {
+    DBG("Network connected");
+  }
+
+#if TINY_GSM_USE_GPRS
+  DBG("Connecting to", apn);
+  if (!modem.gprsConnect(apn, _, _)) {
+    delay(10000);
+    return;
+  }
+
+  bool res = modem.isGprsConnected();
+  DBG("GPRS status:", res ? "connected" : "not connected");
+
+  String cop = modem.getOperator();
+  DBG("Operator:", cop);
+
+#endif
+
+#if defined(TINY_GSM_MODEM_HAS_GPS)
+  modem.enableGPS();
+  String gps_raw = modem.getGPSraw();
+  modem.disableGPS();
+  DBG("GPS raw data:", gps_raw);
+  #endif
+
+#if TINY_GSM_USE_SMS && defined(SMS_TARGET)
+  res = send_SMS()
+
+#endif
+
+#if TINY_GSM_USE_GPRS
+  modem.gprsDisconnect();
+  if (!modem.isGprsConnected()) {
+    DBG("GPRS disconnected");
+  } else {
+    DBG("GPRS disconnect: Failed.");
+  }
+#endif
+
+  // Do nothing forevermore
+  while (true) {
+    modem.maintain();
+  }
+}
+
+int send_SMS(){
+    int result = 0;
+    result = modem.sendSMS(SMS_TARGET, String("Hello from Sackey the most awesome"));
+    DBG("SMS:", result ? "OK" : "fail");
+    return result;
+}
